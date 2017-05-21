5547fe50f018156fbee1a59c26600bea4fc6bb26
Fernando Rodrigues Baroni <fernando@baroni.tech>
2017-05-21 13:41:48 -0300

    Updated 2.3.py to use mylist

--- chapter_2/2.3.py
+++ chapter_2/2.3.py
@@ -1,26 +1,5 @@
 import unittest
-
-class LinkedList:
-    def __init__(s, value):
-        s.data = value
-        s.next = None
-
-def createLinkedList(elements):
-    res = None
-    for element in reversed(elements):
-        e = LinkedList(element)
-        if res is not None:
-            e.next = res
-        res = e
-    return res
-
-def toList(linkedList):
-    res = []
-    n = linkedList
-    while n is not None:
-        res.append(n.data)
-        n = n.next
-    return res
+from mylist import LinkedList
         
 def deleteElement(node):
     node.data = node.next.data
@@ -28,16 +7,16 @@ def deleteElement(node):
 
 class Playground(unittest.TestCase):
     def test_1(self):
-        A = createLinkedList([1, 2, 3])
+        A = LinkedList.create([1, 2, 3])
         E = [1, 3]
         deleteElement(A.next)
-        self.assertEqual(toList(A), E)
+        self.assertEqual(A.toList(), E)
 
     def test_1(self):
-        A = createLinkedList([1, 2, 3])
+        A = LinkedList.create([1, 2, 3])
         E = [2, 3]
         deleteElement(A)
-        self.assertEqual(toList(A), E)
+        self.assertEqual(A.toList(), E)
 
 if __name__ == '__main__':
     unittest.main()

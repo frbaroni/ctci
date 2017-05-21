ac335f8408276c1504cbed48265303ef48645ec3
Fernando Rodrigues Baroni <fernando@baroni.tech>
2017-05-21 13:52:22 -0300

    Problem 2.5

--- chapter_2/2.5.py
+++ chapter_2/2.5.py
@@ -1,29 +1,31 @@
 import unittest
 from mylist import LinkedList
         
-def partition(head, P):
-    n = head
-    while n is not None:
-        if n.data >= P:
-            s = n.next
-            while s is not None:
-                if s.data < P:
-                    tmp = n.data
-                    n.data = s.data
-                    s.data = tmp
-                    break
-                s = s.next
-            if s is None:
-                break
-        n = n.next
-    return head
+def sumLists(a, b):
+    head = LinkedList(None)
+    out = head
+    bit = 0
+    while (a is not None) or (b is not None):
+        va = a.data if a is not None else 0
+        vb = b.data if b is not None else 0
+        r = va + vb + bit
+        bit = 1 if r > 9 else 0
+        r %= 10
+        out.next = LinkedList(r)
+        out = out.next
+        a = a.next
+        b = b.next
+    if bit:
+        out.next = LinkedList(bit)
+        out = out.next
+    return head.next
 
 class Playground(unittest.TestCase):
     def test_1(self):
-        A = [3, 5, 8, 5, 10, 2, 1]
-        P = 5
-        E = [3, 2, 1, 5, 10, 5, 8]
-        self.assertEqual(partition(LinkedList.create(A), P).toList(), E)
+        A = LinkedList.create([7, 1, 6])
+        B = LinkedList.create([5, 9, 2])
+        E = LinkedList.create([2, 1, 9])
+        self.assertEqual(sumLists(A, B).toList(), E.toList())
 
 if __name__ == '__main__':
     unittest.main()

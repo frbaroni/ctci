ac02d62a2bd52e9cfb7216d047364ff6c4f2558f
Fernando Rodrigues Baroni <fernando@baroni.tech>
2017-05-21 13:36:51 -0300

    Updated 2.1.py to use mylist

--- chapter_2/2.1.py
+++ chapter_2/2.1.py
@@ -1,26 +1,5 @@
 import unittest
-
-class LinkedList:
-    def __init__(s, value):
-        s.data = value
-        s.next = None
-
-def createLinkedList(elements):
-    res = None
-    for element in reversed(elements):
-        e = LinkedList(element)
-        if res is not None:
-            e.next = res
-        res = e
-    return res
-
-def toList(linkedList):
-    res = []
-    n = linkedList
-    while n is not None:
-        res.append(n.data)
-        n = n.next
-    return res
+from mylist import LinkedList
         
 # Using set() to hold known elements
 def removeDupsSet(head):
@@ -49,46 +28,48 @@ def removeDups(head):
         n = n.next
     return head
 
+def toList(l):
+    return l.toList()
 
 class Playground(unittest.TestCase):
     def test_nodups(self):
-        A = createLinkedList([1, 2, 3])
-        E = createLinkedList([1, 2, 3])
+        A = LinkedList.create([1, 2, 3])
+        E = LinkedList.create([1, 2, 3])
         self.assertEqual(toList(removeDupsSet(A)), toList(E))
 
     def test_hasdup(self):
-        A = createLinkedList([1, 2, 1, 3])
-        E = createLinkedList([1, 2, 3])
+        A = LinkedList.create([1, 2, 1, 3])
+        E = LinkedList.create([1, 2, 3])
         self.assertEqual(toList(removeDupsSet(A)), toList(E))
 
     def test_lastisdup(self):
-        A = createLinkedList([1, 2, 1, 3, 3])
-        E = createLinkedList([1, 2, 3])
+        A = LinkedList.create([1, 2, 1, 3, 3])
+        E = LinkedList.create([1, 2, 3])
         self.assertEqual(toList(removeDupsSet(A)), toList(E))
 
     def test_firstisdup(self):
-        A = createLinkedList([1, 1, 2, 1, 3])
-        E = createLinkedList([1, 2, 3])
+        A = LinkedList.create([1, 1, 2, 1, 3])
+        E = LinkedList.create([1, 2, 3])
         self.assertEqual(toList(removeDupsSet(A)), toList(E))
 
     def test_2_nodups(self):
-        A = createLinkedList([1, 2, 3])
-        E = createLinkedList([1, 2, 3])
+        A = LinkedList.create([1, 2, 3])
+        E = LinkedList.create([1, 2, 3])
         self.assertEqual(toList(removeDups(A)), toList(E))
 
     def test_2_hasdup(self):
-        A = createLinkedList([1, 2, 1, 3])
-        E = createLinkedList([1, 2, 3])
+        A = LinkedList.create([1, 2, 1, 3])
+        E = LinkedList.create([1, 2, 3])
         self.assertEqual(toList(removeDups(A)), toList(E))
 
     def test_2_lastisdup(self):
-        A = createLinkedList([1, 2, 1, 3, 3])
-        E = createLinkedList([1, 2, 3])
+        A = LinkedList.create([1, 2, 1, 3, 3])
+        E = LinkedList.create([1, 2, 3])
         self.assertEqual(toList(removeDups(A)), toList(E))
 
     def test_2_firstisdup(self):
-        A = createLinkedList([1, 1, 2, 1, 3])
-        E = createLinkedList([1, 2, 3])
+        A = LinkedList.create([1, 1, 2, 1, 3])
+        E = LinkedList.create([1, 2, 3])
         self.assertEqual(toList(removeDups(A)), toList(E))
 
 

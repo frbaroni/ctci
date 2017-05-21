4dd7bbf48987ecaa0790695deb3c3bf739a25b6c
Fernando Rodrigues Baroni <fernando@baroni.tech>
2017-05-21 13:41:52 -0300

    Updated 2.4.py to use mylist

--- chapter_2/2.4.py
+++ chapter_2/2.4.py
@@ -1,27 +1,6 @@
 import unittest
+from mylist import LinkedList
 
-class LinkedList:
-    def __init__(s, value):
-        s.data = value
-        s.next = None
-
-def createLinkedList(elements):
-    res = None
-    for element in reversed(elements):
-        e = LinkedList(element)
-        if res is not None:
-            e.next = res
-        res = e
-    return res
-
-def toList(linkedList):
-    res = []
-    n = linkedList
-    while n is not None:
-        res.append(n.data)
-        n = n.next
-    return res
-        
 def partition(head, P):
     n = head
     while n is not None:
@@ -44,7 +23,7 @@ class Playground(unittest.TestCase):
         A = [3, 5, 8, 5, 10, 2, 1]
         P = 5
         E = [3, 2, 1, 5, 10, 5, 8]
-        self.assertEqual(toList(partition(createLinkedList(A), P)), E)
+        self.assertEqual(partition(LinkedList.create(A), P).toList(), E)
 
 if __name__ == '__main__':
     unittest.main()
